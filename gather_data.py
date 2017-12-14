b18b98f95ad4c1709baa354ba63b90dcc4d7e355
Trent Baur <trentbaur@gmail.com>
2017-12-14 17:08:22 -0500

    Results generating, first 2 submissions made

--- gather_data.py
+++ gather_data.py
@@ -1,5 +1,6 @@
 import pandas as pd
-
+import math 
+import pdb
 
 FOLDERS = {'raw': 'data_raw/',
            'clean': 'data_clean/'}
@@ -16,6 +17,16 @@ def tidy_data(data):
 
     data.columns = [col.lower() for col in data.columns]
 
+    #pdb.set_trace()
+    
+    data.name = data.name.replace('Ms.', 'Miss', regex = True)
+    
+    for title in ('Master', 'Miss', 'Mr.', 'Mrs.', 'Dr'):
+        med = math.ceil(data[(data.name.str.contains(title)) & (data.age.notnull())].age.describe()['50%'])
+        
+        data.loc[data.age.isnull() & data.name.str.contains(title), 'age'] = med
+        
+
     enc_sex = pd.get_dummies(data.sex)
     data = data.join(enc_sex)
     data.drop(['sex'], axis = 1, inplace = True)
@@ -26,6 +37,8 @@ def tidy_data(data):
     data = data.join(enc_embark)
     data.drop(['embarked'], axis = 1, inplace = True)
 
+    data.loc[data.fare.isnull(), 'fare'] = data.fare.describe()['50%']
+        
     data['ticket_class'] = data.ticket.str.replace('[0-9]| ', '')
 
     data['cabin_floor'] = data.cabin.str.replace('[0-9]| ', '').str.get(0)
--- run.py
+++ run.py
@@ -5,7 +5,6 @@ from sklearn.model_selection import train_test_split
 #   Gather training data 
 #----------------------------
 train = gather_data.load_data('train.csv')
-#train.dropna(subset = ['Age'], inplace = True)
 
 
 
@@ -13,6 +12,21 @@ train = gather_data.load_data('train.csv')
 #   Summarize / Explore Data
 #------------------------------
 #   train.groupby('Survived').count()
+'''
+#   Explore records with no age value
+train[train.age.isnull()]
+
+name_stub = 'Master'
+
+train[train.Name.str.contains(name_stub)]
+
+import matplotlib.pyplot as plt
+plt.hist(train[(train.Name.str.contains('Mrs')) & (train.Age.notnull())].Age, bins=10)
+
+plt.hist(train[(train.age > 0)].fare, bins=10)
+
+
+'''
 
 
 
@@ -37,12 +51,21 @@ X_train, X_dev, y_train, y_dev = train_test_split(train[cols_to_model],
 #--------------------------------------------------
 #   Prepare model and train on data
 #--------------------------------------------------
+'''
 from sklearn.linear_model import LogisticRegression
 
 model = LogisticRegression(C = 1)
 
 model.fit(X_train, y_train)
+'''
 
+from sklearn.ensemble import RandomForestClassifier
+
+model = RandomForestClassifier(n_estimators = 1000,
+                               max_features = 3,
+                               random_state = 0)
+
+model.fit(X_train, y_train)
 
 #--------------------------------------------------
 #   Generate metrics to evaluate model performance
@@ -63,11 +86,28 @@ print(classification_report (y_dev,
 #   Run test data through model and return results for submitting
 #--------------------------------------------------------------------
 test = gather_data.load_data('test.csv')
-test.dropna()
-test = test[test.Age.notnull() & test.Fare.notnull()]
+#   test[test.fare.isnull()]
 
 x_test = gather_data.tidy_data(test)
 x_test = x_test[cols_to_model]
 
-model.predict(x_test)
+x_test[x_test.age.isnull()]
+x_test[x_test.fare.isnull()]
+
+y_pred = model.predict(x_test)
+
+res = zip(test.passengerid, y_pred)
+
+import csv
+
+csvfile = "randomforest.csv"
+
+with open(csvfile, "w") as output:
+    writer = csv.writer(output, lineterminator='\n')
+    writer.writerow(['PassengerId', 'Survived'])
+    for line in res:
+        id = line[0]
+        value = line[1]
+        writer.writerow([id, value])
+        
 
